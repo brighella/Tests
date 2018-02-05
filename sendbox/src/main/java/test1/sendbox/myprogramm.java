d7786a3e47ddcc22a909ca61ab0efcf881be537a
brighella <olga.kozhukhovskaya.93@mail.ru>
2018-02-05 15:32:16 +0300

    new function

--- sendbox/src/main/java/test1/sendbox/myprogramm.java
+++ sendbox/src/main/java/test1/sendbox/myprogramm.java
@@ -2,20 +2,18 @@ package test1.sendbox;
 
 public class Myprogramm {
 
-  public static void main(String[] args)
-  {
-hello("world");
-double len = 5;
+  public static void main(String[] args)  {
+    hello("world");
 
-    System.out.println("площадь кв со стороной " + len + "=" + area(len));
+    Square s = new Square(5);
+    System.out.println("площадь кв со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,5);
+    System.out.println("площадь прямоугольника со сторонами " + r.a +" и " + r.b + " = " + r.area());
+
   }
-  public static void hello (String somebody)
-  {
+  public static void hello (String somebody)  {
     System.out.println("Hello," + somebody + "!");
-  }
-  public static double area (double l) {
-
-    return l * l;
   }
 
 }
